f7ea1be56cfa520abf28995e71552ad08159a784
baferdows <you@example.com>
2025-04-13 09:47:05 -0500

    Fix JSX syntax in Route element for PropertyInputPage

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import HomePage from "./pages/HomePage";
-import LogoutButton from "./components/LogoutButton";
-import WelcomeMessage from "./components/WelcomeMessage";
-import PropertyInputPage from "./pages/PropertyInputPage";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./src/pages/LoginPage";
+import RegisterPage from "./src/pages/RegisterPage";
+import HomePage from "./src/pages/HomePage";
+import LogoutButton from "./src/components/LogoutButton";
+import WelcomeMessage from "./src/components/WelcomeMessage";
+import PropertyInputPage from "./src/pages/PropertyInputPage";
 
 export default function App() {
   return (
-    <Router>
-      <div className="p-4">
-        <div className="flex justify-between items-center mb-4">
-          <WelcomeMessage />
-          <LogoutButton />
-        </div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/input" element={<PropertyInputPage />} />
-        </Routes>
+    <div className="p-4">
+      <div className="flex justify-between items-center mb-4">
+        <WelcomeMessage />
+        <LogoutButton />
       </div>
-    </Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/input" element={<PropertyInputPage />} />
+      </Routes>
+    </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import "./style.css"; // or whatever CSS file you're using
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
